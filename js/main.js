271cf4765483f697b9e492561c02274319f6a710
zeekomkommer <anouaranthonytel@gmail.com>
2024-11-08 22:46:31 +0100

    Nearly Done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,62 @@
-console.log('Happy developing ✨')
+let turnX = true;
+let positions = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+let playing = true;
+
+const winCombos = [
+    ["0", "1", "2"],
+    ["3", "4", "5"],
+    ["6", "7", "8"],
+    ["0", "3", "6"],
+    ["1", "4", "7"],
+    ["2", "5", "8"],
+    ["0", "4", "8"],
+    ["2", "4", "6"]
+];
+
+
+function ticPosition(button) {
+    if (positions.includes(button.id) && playing) {
+        document.getElementById(button.id).classList.remove("empty");
+        if (turnX) {
+            document.getElementById(button.id).classList.add("x");
+            positions[button.id] = "x";
+        } else {
+            document.getElementById(button.id).classList.add("o");
+            positions[button.id] = "o";
+        }
+        if (winCheck()) {
+            if (turnX) {
+                playing = false;
+                console.log("Player X won");
+            } else {
+                playing = false;
+                console.log("Player O won");
+            }
+        }
+        turnX = !turnX;
+    }
+}
+
+function winCheck() {
+    for (const combo of winCombos) {
+        if (positions[combo[0]] === positions[combo[1]] && positions[combo[1]] === positions[combo[2]]) {
+            document.getElementById(combo[0]).classList.add("green");
+            document.getElementById(combo[1]).classList.add("green");
+            document.getElementById(combo[2]).classList.add("green");
+            return true;
+        }
+    }
+    return false;
+}
+
+function reset() {
+    positions = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+    playing = true;
+    turnX = true;
+    positions.forEach(id => {
+        document.getElementById(id).classList.remove("o");
+        document.getElementById(id).classList.remove("x");
+        document.getElementById(id).classList.remove("green");
+        document.getElementById(id).classList.add("empty");
+    });
+}
